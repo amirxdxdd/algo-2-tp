c63642ef6315af75693e01e1ee48a87a70e4e783
unknown <amircab15@gmail.com>
2026-05-17 20:49:53 -0300

    hice muchas cosas, falta front

--- logica.py
+++ logica.py
@@ -1,76 +1,75 @@
 import pandas as pd
-def ingresar_equipo():
-    df = pd.read_excel("data/equipos.xlsx")
+def calcular_stats():
+    equipos  = pd.read_excel("data/equipos.xlsx")
+    partidos = pd.read_excel("data/partidos.xlsx")
+    stats = equipos.copy()  # copiamos la tabla de equipos, no modifico el excel
 
-    id = int(input("ID: "))
+    stats["pj"] = 0     #estas columnas son solamente auxiliares
+    stats["gf"] = 0
+    stats["gc"] = 0
+    stats["puntos"] = 0
+
+    for eid in equipos["id"]:  #recorre todos los equipos
+
+        local = partidos[partidos["equipo1"] == eid]    #con local y visitante me refiero a los partidos
+        visitante = partidos[partidos["equipo2"] == eid] #en donde el equipo fue el equipo 1 o el 2
+
+        pj = len(local) + len(visitante)
+        gf = int(local["goles1"].sum()) + int(visitante["goles2"].sum())
+        gc = int(local["goles2"].sum()) + int(visitante["goles1"].sum())
+
+
+
+        #las comparaciones devuelven una serie de pandas, de true o false, .sum da la cantidad de trues 
+        puntos = int((local["goles1"]     > local["goles2"]).sum())     * 3     #si la condicion es cierta 
+        puntos+= int((local["goles1"]    == local["goles2"]).sum())     * 1     #multiplico por 3 si es que gano y
+        puntos+= int((visitante["goles2"] > visitante["goles1"]).sum()) * 3     #por 1 si es que empato
+        puntos+= int((visitante["goles2"] == visitante["goles1"]).sum()) * 1
+
+        # asignar los valores
+        stats.loc[stats["id"] == eid, "pj"]     = pj
+        stats.loc[stats["id"] == eid, "gf"]     = gf
+        stats.loc[stats["id"] == eid, "gc"]     = gc
+        stats.loc[stats["id"] == eid, "puntos"] = puntos
+
+    stats["dg"] = stats["gf"] - stats["gc"]
+
+    return stats
+
+def ingresar_equipo():
+    df = pd.read_excel("data/equipos.xlsx")     
+    #para ingresar los equipos, solo son necesarios estos datos, no es necesario preocuparse por los puntos, de momento
+    #cuando vaya a ver los equipos necesitare de la funcion que calcule las estadisticas para poder verlos en orden
+    id = input("ID: ")
     pais = input("País: ")
     grupo = input("Grupo: ")
     prefijo = int(input("Prefijo: "))
     confederacion = input("Confederación: ")
 
-    df.loc[len(df)]=[id,pais,grupo,prefijo,confederacion,0,0,0,0]   #pone al equipo al final
-    df.to_excel("data/equipos.xlsx", index=False)
+    df.loc[len(df)]=[id, pais, grupo, prefijo, confederacion] 
+    df.to_excel("data/equipos.xlsx", index=False)   #se envia de esta forma para que no ponga indices
     print("Equipo agregado correctamente")
 
 def ver_equipos():
 
-    df = pd.read_excel("data/equipos.xlsx")
-
-    # diferencia de gol 
-    df["dg"] = df["gf"] - df["gc"]
-
-    
+    df = calcular_stats()
     df = df.sort_values(
         by=["puntos", "dg", "gf", "prefijo"],       #ordena antes de imprimir los equipos siguiendo los 4 criterios
-        ascending=[False, False, False, False]
+        ascending=[False, False, False, False] #en cualquier caso se ordena de forma descendente
     )
 
-    print(df.to_string(index=False))
+    print(df.to_string(index=False))    #se tiene que imprimir de esta forma para que no imprima los indices
 
 def ingresar_partido():
-    df_eq = pd.read_excel("data/equipos.xlsx")
-    df_eq["pj"] = df_eq["pj"].astype(int)
-    df_eq["gf"] = df_eq["gf"].astype(int)
-    df_eq["gc"] = df_eq["gc"].astype(int)
-    df_eq["puntos"] = df_eq["puntos"].astype(int)
-    df_par= pd.read_excel("data/partidos.xlsx")
-    fecha = input("Ingrese la fecha DD/MM/AAAA: ")
-    eq1=int(input("ID del equipo 1: "))
-    eq2=int(input("ID del equipo 2: "))
-
-    g1=int(input("Goles equipo 1: "))
-    g2=int(input("Goles del equipo 2: "))
-
-    df_par.loc[len(df_par)]=[fecha,eq1,eq2,g1,g2]
-
-    #busca las filas de los equipos
-    i1 = df_eq[df_eq["id"] == eq1].index[0]
-    i2 = df_eq[df_eq["id"] == eq2].index[0]
+    df_par = pd.read_excel("data/partidos.xlsx")
     
-    #Actualizar los partidos jugados
-    df_eq.loc[i1, "pj"] += 1
-    df_eq.loc[i2, "pj"] += 1
-
-    # actualizar goles a favor y en contra
-    df_eq.loc[i1, "gf"] += g1
-    df_eq.loc[i1, "gc"] += g2
-
-    df_eq.loc[i2, "gf"] += g2
-    df_eq.loc[i2, "gc"] += g1
-
-    # actualizar puntos
+    fecha = input("Fecha DD/MM/AAAA: ")
+    eq1 = input("ID equipo 1: ").upper()
+    eq2 = input("ID equipo 2: ").upper()
+    g1 = int(input("Goles equipo 1: "))
+    g2 = int(input("Goles equipo 2: "))
     
-    if g1 > g2:
-        df_eq.loc[i1, "puntos"] += 3       #Si el eq1 gana
-
-    elif g1 == g2:
-        df_eq.loc[i1, "puntos"] += 1    #Si empatan
-        df_eq.loc[i2, "puntos"] += 1
-
-    else:
-        df_eq.loc[i2, "puntos"] += 3    #Si el eq2 gana
-
-    df_eq.to_excel("data/equipos.xlsx", index=False)
+    df_par.loc[len(df_par)] = [fecha, eq1, eq2, g1, g2]
     df_par.to_excel("data/partidos.xlsx", index=False)
     print("Partido agregado correctamente")
 
